2055df85f568d14bee7aa5f740dc8b143bf5f350
Austin Luft <ausluft@gmail.com>
2018-09-25 04:20:14 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,7 @@ def second_challenge
   }
 
   #code your solution here!
-groceries.values[0].concat(groceries.values[1]).concat(groceries.values[2])
+groceries.values[0].concat(groceries.values[1]).concat(groceries.values[2]).concat(groceries.values[3])
   
 
 end
