ad4db369e19684159f61161f83057c2813c9330f
Austin Luft <ausluft@gmail.com>
2018-09-25 03:59:51 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,9 @@ def first_challenge
   }
 
   #your code here
-
+contacts["Freddy Mercury"][:favorite_icecream_flavors].each do |flavor|
+  flavor.delete_if == "strawberry"
+end
 
   #remember to return your newly altered contacts hash!
   contacts
