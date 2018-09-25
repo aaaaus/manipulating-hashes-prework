054ead1e31ed1adcb269998ffc9475c5d46ed7e2
Austin Luft <ausluft@gmail.com>
2018-09-25 04:23:17 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,11 @@ def first_challenge
   }
 
   #your code here
-
+contacts["Freddy Mercury"][:favorite_icecream_flavors].collect do |flavor|
+  if flavor == "strawberry"
+    contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
+  end
+end
 
   #remember to return your newly altered contacts hash!
   contacts
